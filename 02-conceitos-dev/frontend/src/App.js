21fe747c3eafe9388e57a0c353221fc05b51dfc2
Milene Taborda <milenetaborda1@outlook.com>
2021-03-08 16:08:28 -0300

    feat: listing API projects

--- 02-conceitos-dev/frontend/src/App.js
+++ 02-conceitos-dev/frontend/src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
 import Header from "./components/Header";
 
 import "./App.css";
-import backgroundImage from "./assets/photo.jpg"
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
 
   function handleAddProject(){
     setProjects([...projects, `Novo projeto, ${Date.now()}`])
@@ -15,10 +22,8 @@ function App() {
     <>
       <Header title="Homepage" />
 
-      <img src={backgroundImage} width={300} />
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
