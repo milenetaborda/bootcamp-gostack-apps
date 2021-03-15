089a32736fd0215ebe847f8258ecbeedc1bfd615
Milene Taborda <milenetaborda1@outlook.com>
2021-03-15 18:38:55 -0300

    chore: Initial Commit

---
+++ 02-conceitos-dev/mobile/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, StyleSheet} from "react-native";
+
+export default function App() {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7139c1'
+  }
+})
