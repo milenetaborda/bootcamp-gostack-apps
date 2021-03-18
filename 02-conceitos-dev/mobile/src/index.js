3c30898371d93a62659511443a24b107c966c71a
Milene Taborda <milenetaborda1@outlook.com>
2021-03-18 20:51:54 -0300

    feat: listing api projects

--- 02-conceitos-dev/mobile/src/index.js
+++ 02-conceitos-dev/mobile/src/index.js
@@ -1,14 +1,29 @@
-import React from "react";
-import {View, StyleSheet, StatusBar} from "react-native";
+import React, { useEffect, useState } from "react";
+import {SafeAreaView, FlatList, StyleSheet, StatusBar} from "react-native";
+import api from "./services/api";
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1">
-        <View style={styles.container} >
-          <Text style={styles.title}>Hello GoStack</Text>
-        </View>
-      </StatusBar>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.title}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 };
@@ -16,9 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7139c1',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#7139c1'
   },
 
   title: {
