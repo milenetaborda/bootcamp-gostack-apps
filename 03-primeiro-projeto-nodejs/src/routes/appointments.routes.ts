cc2db2a4901854eb2752cc8cb71c68846edeb13b
Milene Taborda <milenetaborda1@outlook.com>
2021-03-26 05:02:11 -0300

    refactor: creating repositories

--- 03-primeiro-projeto-nodejs/src/routes/appointments.routes.ts
+++ 03-primeiro-projeto-nodejs/src/routes/appointments.routes.ts
@@ -1,26 +1,22 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns'
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const parseDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parseDate, appointment.date),
-  );
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate)
 
   if(findAppointmentInSameDate) {
     return response.status(400).json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parseDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
