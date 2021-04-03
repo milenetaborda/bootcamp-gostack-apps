411fa28e759a5a97fd63a6c7c452a30e51e5978f
Milene Taborda <milenetaborda1@outlook.com>
2021-04-03 00:12:15 -0300

    feat: model relationships

--- 03-primeiro-projeto-nodejs/src/database/migrations/1617415386272-CreateUsers.ts
+++ 03-primeiro-projeto-nodejs/src/database/migrations/1617415386272-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1617415386272 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
